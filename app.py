1418f69ea99db6f4c9e199c3eea633b320226739
Samuel Murillo <samuelmurillo333@gmail.com>
2020-08-07 20:31:26 -0300

    Use docker-py in html rendering

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from typing import List
 
+import docker
 import aiodocker
+from docker.models.containers import Container
 from aiodocker.containers import DockerContainer
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import HTMLResponse
@@ -10,37 +12,26 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"))
 
+docker_client = docker.from_env()
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    container_list_data = []
-    async with aiodocker.Docker() as docker:
-        container_list: List[DockerContainer] = await docker.containers.list()
-        for container in container_list:
-            container_metadata = await container.show()
-            container_list_data.append({
-                "name": container_metadata.get("Name")[1:],
-                "id": container_metadata.get("Id")[:12],
-                "image": container_metadata.get("Config").get("Image"),
-            })
-            context = {
-                "request": request,
-                "container_list": container_list_data,
-            }
+    container_list: List[Container] = docker_client.containers.list()
+    context = {
+        "request": request,
+        "container_list": container_list,
+    }
     return templates.TemplateResponse("index.html", context)
 
 
 @app.get("/container/{container_id}/", response_class=HTMLResponse)
 async def container_view(request: Request, container_id: str):
-    async with aiodocker.Docker() as docker:
-        container = await docker.containers.get(container_id)
-        container_metadata = await container.show()
+    container = docker_client.containers.get(container_id)
     context = {
         "request": request,
-        "container_id": container_id,
-        "container_name": container_metadata.get("Name")[1:],
+        "container": container
     }
     return templates.TemplateResponse("container.html", context)
 
@@ -49,7 +40,7 @@ async def container_view(request: Request, container_id: str):
 async def ws(websocket: WebSocket, container_id: str):
     await websocket.accept()
     async with aiodocker.Docker() as docker:
-        container = await docker.containers.get(container_id)
+        container: DockerContainer = await docker.containers.get(container_id)
         logs = container.log(stdout=True, stderr=True, follow=True, tail=50)
         async for line in logs:
             await websocket.send_text(line)
